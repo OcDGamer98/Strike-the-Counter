b678aece370bc8b096526e281c4c5d1d4b46426a
Juan Sanchez <generalr98@gmail.com>
2015-09-20 19:22:32 -0500

    Updated for collaborators

---
+++ Assets/Scripts/debugMenu.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var debugMenu : GameObject; 
+
+function Start () {
+    debugMenu.SetActive(false);
+}
+
+function Update () {
+var isEnabled = false;
+    if(Input.GetKey(KeyCode.LeftControl)&&Input.GetKey(KeyCode.LeftShift)&&Input.GetKey(KeyCode.F1)){
+		debugMenu.SetActive(true);
+		if(isEnabled == true){
+			debugMenu.SetActive(false);
+		}
+		isEnabled = !isEnabled;
+    }
+}
